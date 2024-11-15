efb428a75330f8cc6ab5fbdcf9a9b86051a37264
XLopchik13 <nikita.petrov.it@gmail.com>
2024-11-15 20:46:17 +0300

    Initial commit

---
+++ app/api/router.py
@@ -0,0 +1,87 @@
+import base64
+
+from aiogram.types import BufferedInputFile
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.api.schemas import QRCodeRequest, QRCodeScaner
+from app.bot.create_bot import bot
+from app.bot.keyboards.kbs import main_keyboard
+
+
+router = APIRouter(
+    prefix='/api',
+    tags=['API']
+)
+
+
+@router.post("/send-qr/", response_class=JSONResponse)
+async def send_qr_code(request: QRCodeRequest):
+    try:
+        base64_data = request.qr_code_url
+
+        if base64_data.startswith('data:image/'):
+            base64_data = base64_data.split(',', 1)[1]
+
+        image_data = base64.b64decode(base64_data)
+
+        image_file = BufferedInputFile(
+            file=image_data,
+            filename="qr_code.png"
+        )
+
+        caption = (
+            "🎉 Ваш QR-код успешно создан и отправлен!\n\n"
+            "🔍 Вы можете отсканировать его, чтобы проверить содержимое.\n"
+            "📤 Поделитесь этим QR-кодом с другими или сохраните его для дальнейшего использования.\n\n"
+            "Что бы вы хотели сделать дальше? 👇"
+        )
+
+        await bot.send_photo(
+            chat_id=request.user_id,
+            photo=image_file,
+            caption=caption,
+            reply_markup=main_keyboard()
+        )
+
+        return JSONResponse(
+            content={"message": "QR-код успешно отправлен"},
+            status_code=200
+        )
+
+    except Exception as e:
+        print(f"Ошибка при отправке QR-кода: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Ошибка при отправке QR-кода: {str(e)}"
+        )
+
+
+@router.post("/send-scaner-info/", response_class=JSONResponse)
+async def send_qr_code(request: QRCodeScaner):
+    try:
+        text = (
+            f"🎉 QR-код успешно отсканирован!\n\n"
+            f"📄 Результат сканирования:\n\n"
+            f"<code><b>{request.result_scan}</b></code>\n\n"
+            f"🔗 Если это ссылка, вы можете перейти по ней.\n"
+            f"📝 Если это текст, вы можете скопировать его для дальнейшего использования.\n\n"
+            f"Что бы вы хотели сделать дальше? 👇"
+        )
+
+        await bot.send_message(
+            chat_id=request.user_id,
+            text=text,
+            reply_markup=main_keyboard()
+        )
+
+        return JSONResponse(
+            content={"message": "QR-код успешно просканирован, а данные отправлены в Telegram"},
+            status_code=200
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
---
+++ app/pages/router.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+router = APIRouter(
+    prefix='',
+    tags=['Frontend']
+)
+templates = Jinja2Templates(directory='app/templates')
+
+
+@router.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "pages/generate.html",
+        {"request": request, 'active_tab': 'generate'}
+    )
+
+
+@router.get("/scan", response_class=HTMLResponse)
+async def scan_qr(request: Request):
+    return templates.TemplateResponse(
+        "pages/scan.html",
+        {"request": request, 'active_tab': 'scan'}
+    )
+
+
+@router.get("/upload", response_class=HTMLResponse)
+async def upload_qr(request: Request):
+    return templates.TemplateResponse(
+        "pages/upload.html",
+        {"request": request, 'active_tab': 'upload'}
+    )
